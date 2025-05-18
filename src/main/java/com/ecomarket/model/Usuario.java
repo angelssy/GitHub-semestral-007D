e0a65b204e3f6ab588517475b5ff6b7d62c81bad
ROYALE <an.lazaro@duocuc.cl>
2025-05-18 19:43:53 -0400

    agregamos los atributos a la clase usuario

--- src/main/java/com/ecomarket/model/Usuario.java
+++ src/main/java/com/ecomarket/model/Usuario.java
@@ -1,4 +1,21 @@
 package com.ecomarket.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String password;
+
 }
