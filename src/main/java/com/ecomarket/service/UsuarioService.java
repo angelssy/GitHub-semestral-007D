31167333e06ef1e93e29f9ef6557ce38c8036696
ROYALE <an.lazaro@duocuc.cl>
2025-05-19 11:09:04 -0400

    agregamos los el service

--- src/main/java/com/ecomarket/service/UsuarioService.java
+++ src/main/java/com/ecomarket/service/UsuarioService.java
@@ -1,4 +1,37 @@
 package com.ecomarket.service;
 
+import com.ecomarket.model.Usuario;
+import com.ecomarket.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getUsuarios() {
+        return usuarioRepository.obtenerUsuarios();
+
+    }
+
+    public Usuario saveUsuario(Usuario usuario) {
+        return usuarioRepository.guardar(usuario);
+
+    }
+    public Usuario getUsuario(int id) {
+        return usuarioRepository.buscarPorId(id);
+
+    }
+    public Usuario updateUsuario(Usuario usuario) {
+        return usuarioRepository.actualizar(usuario);
+    }
+
+    public String deleteLibro(int id) {
+        usuarioRepository.eliminar(id);
+        return "Usuario eliminado";
+    }
+
 }
