01005e7fdbbf66df3793aa250bc368f92a6af70b
ROYALE <an.lazaro@duocuc.cl>
2025-07-13 23:20:05 -0400

    agregamos el swagger

--- src/main/java/com/ecomarket/config/SwaggerConfig.java
+++ src/main/java/com/ecomarket/config/SwaggerConfig.java
@@ -1,24 +1,29 @@
 package com.ecomarket.config;
 
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI ecomarketOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("EcoMarket API")
+                        .description("API para la gestión de productos ecológicos en ecomarket")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("EcoMarket Team")
+                                .url("https://ecomarket.com")
+                                .email("info@ecomarket.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
     }
+
 }
